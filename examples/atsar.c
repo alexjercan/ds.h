5b2d6d4e855eff3141c550941a855768eb579c13
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-11 09:15:29 +0200

    format

--- examples/atsar.c
+++ examples/atsar.c
@@ -1,22 +1,22 @@
 #include <limits.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define DS_PQ_IMPLEMENTATION
 #include "../ds.h"
 
 #define LINE_MAX 1024
 
 struct lines {
-    char **items;
-    int count;
-    int capacity;
+        char **items;
+        int count;
+        int capacity;
 };
 
 struct world {
-    int width;
-    int height;
-    unsigned char *map;
+        int width;
+        int height;
+        unsigned char *map;
 };
 
 int world_init(struct world *w, int width, int height) {
@@ -137,9 +137,12 @@ int world_free(struct world *w) {
 }
 
 struct position {
-    int x;
-    int y;
+        int x;
+        int y;
 };
+
+const struct position directions[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+const int num_directions = sizeof(directions) / sizeof(directions[0]);
 
 int manhattan_distance(struct position p1, struct position p2) {
     return abs(p1.x - p2.x) + abs(p1.y - p2.y);
@@ -153,136 +156,145 @@ int position_equals(struct position p1, struct position p2) {
     return p1.x == p2.x && p1.y == p2.y;
 }
 
-struct neighbors {
-    struct position *items;
-    int count;
-    int capacity;
+struct position_node {
+        struct position p;
+        int f;
 };
 
-int neighbors_get(struct world *w, struct position *p, struct neighbors *n) {
-    if (p->x > 0 && w->map[p->y * w->width + p->x - 1] == 0) {
-        struct position left = {p->x - 1, p->y};
-        ds_da_append(n, left);
-    }
-
-    if (p->x < w->width - 1 && w->map[p->y * w->width + p->x + 1] == 0) {
-        struct position right = {p->x + 1, p->y};
-        ds_da_append(n, right);
-    }
-
-    if (p->y > 0 && w->map[(p->y - 1) * w->width + p->x] == 0) {
-        struct position up = {p->x, p->y - 1};
-        ds_da_append(n, up);
-    }
-
-    if (p->y < w->height - 1 && w->map[(p->y + 1) * w->width + p->x] == 0) {
-        struct position down = {p->x, p->y + 1};
-        ds_da_append(n, down);
-    }
-
-    return 0;
+struct position_node *position_node_new(struct position p, int f) {
+    struct position_node *node =
+        (struct position_node *)malloc(sizeof(struct position_node));
+    node->p = p;
+    node->f = f;
+    return node;
 }
 
-struct position_node {
-    struct position p;
-    int f;
-};
-
-int simple_node_compare_min(const void *a, const void *b) {
+int position_node_compare_min(const void *a, const void *b) {
     return ((struct position_node *)b)->f - ((struct position_node *)a)->f;
 }
 
 struct path {
-    struct position *items;
-    int count;
-    int capacity;
+        struct position *items;
+        int count;
+        int capacity;
 };
 
-int reconstruct_path(struct world *w, int *came_from, struct position current, struct path *p) {
+int reconstruct_path(struct world *w, int *came_from, struct position current,
+                     struct path *p) {
     ds_da_append(p, current);
     int current_index = position_hash(w, current);
 
     while (came_from[current_index] != -1) {
         current_index = came_from[current_index];
-        struct position current = {current_index % w->width, current_index / w->width};
+        struct position current = {current_index % w->width,
+                                   current_index / w->width};
         ds_da_append(p, current);
     }
 
     return 0;
 }
 
-int a_star(struct world *w, struct position start, struct position end, struct path *p) {
-    struct ds_priority_queue open_set;
-    struct position_node *start_node = (struct position_node *)malloc(sizeof(struct position_node));
-    start_node->p = start;
-    start_node->f = manhattan_distance(start, end);
-    ds_priority_queue_init(&open_set, simple_node_compare_min);
-    ds_priority_queue_insert(&open_set, start_node);
-
+int a_star(struct world *w, struct position start, struct position end,
+           struct path *p) {
+    int result = 0;
     int num_nodes = w->width * w->height;
 
+    // The set of discovered nodes that may need to be (re-)expanded.
+    // Initially, only the start node is known.
+    // This is usually implemented as a min-heap or priority queue rather than a
+    // hash-set.
+    struct ds_priority_queue open_set;
+    ds_priority_queue_init(&open_set, position_node_compare_min);
+
+    struct position_node *start_node =
+        position_node_new(start, manhattan_distance(start, end));
+    ds_priority_queue_insert(&open_set, start_node);
+
+    // For node n, cameFrom[n] is the node immediately preceding it on the
+    // cheapest path from the start to n currently known.
     int *came_from = (int *)malloc(num_nodes * sizeof(int));
     for (int i = 0; i < num_nodes; i++) {
         came_from[i] = -1;
     }
 
+    // For node n, gScore[n] is the cost of the cheapest path from start to n
+    // currently known.
     int *g_score = (int *)malloc(num_nodes * sizeof(int));
     for (int i = 0; i < num_nodes; i++) {
         g_score[i] = INT_MAX;
     }
     g_score[position_hash(w, start)] = 0;
 
+    // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our
+    // current best guess as to how cheap a path could be from start to finish
+    // if it goes through n.
     int *f_score = (int *)malloc(num_nodes * sizeof(int));
     for (int i = 0; i < num_nodes; i++) {
         f_score[i] = INT_MAX;
     }
     f_score[position_hash(w, start)] = manhattan_distance(start, end);
 
-    int result = 0;
+    struct position_node *current_node = NULL;
     while (ds_priority_queue_empty(&open_set) == 0) {
-        struct position_node *current_node = NULL;
+        // This operation can occur in O(Log(N)) time if openSet is a min-heap
+        // or a priority queue
+        current_node = NULL;
         ds_priority_queue_pull(&open_set, (void **)&current_node);
+        int current_index = position_hash(w, current_node->p);
+
+        struct position current = current_node->p;
 
         if (position_equals(current_node->p, end)) {
-            result = 1;
             reconstruct_path(w, came_from, current_node->p, p);
-            free(current_node);
-            break;
+            return_defer(1);
         }
 
-        struct neighbors n = {0};
-        neighbors_get(w, &current_node->p, &n);
-        int current_index = position_hash(w, current_node->p);
-        for (int i = 0; i < n.count; i++) {
-            int neighbor_index = position_hash(w, n.items[i]);
+        for (int i = 0; i < num_directions; i++) {
+            struct position neighbor = {current.x + directions[i].x,
+                                        current.y + directions[i].y};
+            int neighbor_index = position_hash(w, neighbor);
+
+            if (neighbor.x < 0 || neighbor.x >= w->width || neighbor.y < 0 ||
+                neighbor.y >= w->height || w->map[neighbor_index] != 0) {
+                continue;
+            }
+
+            // d(current,neighbor) is the weight of the edge from current to
+            // neighbor tentative_gScore is the distance from start to the
+            // neighbor through current
             int tentative_g_score = g_score[current_index] + 1;
             if (tentative_g_score < g_score[neighbor_index]) {
+                // This path to neighbor is better than any previous one.
                 came_from[neighbor_index] = current_index;
                 g_score[neighbor_index] = tentative_g_score;
-                f_score[neighbor_index] = tentative_g_score + manhattan_distance(n.items[i], end);
+                f_score[neighbor_index] =
+                    tentative_g_score + manhattan_distance(neighbor, end);
 
                 int found = 0;
                 for (unsigned int j = 0; j < open_set.count; j++) {
-                    struct position_node *node = (struct position_node *)open_set.items[j];
-                    if (position_equals(node->p, n.items[i])) {
+                    struct position_node *node =
+                        (struct position_node *)open_set.items[j];
+                    if (position_equals(node->p, neighbor)) {
                         found = 1;
                         break;
                     }
                 }
 
                 if (found == 0) {
-                    struct position_node *neighbor_node = (struct position_node *)malloc(sizeof(struct position_node));
-                    neighbor_node->p = n.items[i];
-                    neighbor_node->f = f_score[neighbor_index];
+                    struct position_node *neighbor_node =
+                        position_node_new(neighbor, f_score[neighbor_index]);
                     ds_priority_queue_insert(&open_set, neighbor_node);
                 }
             }
         }
 
-        free(n.items);
         free(current_node);
     }
 
+defer:
+    if (current_node != NULL) {
+        free(current_node);
+    }
     while (ds_priority_queue_empty(&open_set) == 0) {
         struct position_node *node = NULL;
         ds_priority_queue_pull(&open_set, (void **)&node);
