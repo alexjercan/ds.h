1accc7e3796a12ec31d89959f06e246c6242f616
Alex Jercan <jercan_alex27@yahoo.com>
2024-03-23 18:10:04 +0200

    feat: add argparse

---
+++ examples/argparse.c
@@ -0,0 +1,55 @@
+#define DS_AP_IMPLEMENTATION
+#include "../ds.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int result = 0;
+    ds_argparse_parser parser;
+    ds_argparse_parser_init(&parser, "test", "test description", "0.0.1");
+
+    ds_argparse_add_argument(&parser, (ds_argparse_options){
+                                      .short_name = 'x',
+                                      .long_name = "number",
+                                      .description = "some number input",
+                                      .type = ARGUMENT_TYPE_VALUE,
+                                      .required = 1,
+                                  });
+    ds_argparse_add_argument(&parser, (ds_argparse_options){
+                                      .short_name = 'c',
+                                      .long_name = "config",
+                                      .description = "config file",
+                                      .type = ARGUMENT_TYPE_POSITIONAL,
+                                      .required = 1,
+                                  });
+    ds_argparse_add_argument(&parser, (ds_argparse_options){
+                                      .short_name = 'f',
+                                      .long_name = "file",
+                                      .description = "just a file",
+                                      .type = ARGUMENT_TYPE_POSITIONAL,
+                                      .required = 0,
+                                  });
+    ds_argparse_add_argument(&parser, (ds_argparse_options){
+                                      .short_name = 'o',
+                                      .long_name = "output",
+                                      .description = "optional output file",
+                                      .type = ARGUMENT_TYPE_VALUE,
+                                      .required = 0,
+                                  });
+
+    if (ds_argparse_parse(&parser, argc, argv) != 0) {
+        return_defer(1);
+    }
+
+    char *number = ds_argparse_get_value(&parser, "number");
+    char *file = ds_argparse_get_value(&parser, "file");
+
+    if (number) {
+        printf("number: %s\n", number);
+    }
+
+    printf("file: %s\n", file);
+
+defer:
+    ds_argparse_free(&parser);
+    return result;
+}
