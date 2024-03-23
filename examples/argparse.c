6a14648ec811d328575324e7067fcb4e31ba9c61
Alex Jercan <jercan_alex27@yahoo.com>
2024-03-23 18:45:38 +0200

    feat: implement rest positional

--- examples/argparse.c
+++ examples/argparse.c
@@ -8,33 +8,40 @@ int main(int argc, char *argv[]) {
     ds_argparse_parser_init(&parser, "test", "test description", "0.0.1");
 
     ds_argparse_add_argument(&parser, (ds_argparse_options){
-                                      .short_name = 'x',
-                                      .long_name = "number",
-                                      .description = "some number input",
-                                      .type = ARGUMENT_TYPE_VALUE,
-                                      .required = 1,
-                                  });
+                                          .short_name = 'x',
+                                          .long_name = "number",
+                                          .description = "some number input",
+                                          .type = ARGUMENT_TYPE_VALUE,
+                                          .required = 1,
+                                      });
     ds_argparse_add_argument(&parser, (ds_argparse_options){
-                                      .short_name = 'c',
-                                      .long_name = "config",
-                                      .description = "config file",
-                                      .type = ARGUMENT_TYPE_POSITIONAL,
-                                      .required = 1,
-                                  });
+                                          .short_name = 'c',
+                                          .long_name = "config",
+                                          .description = "config file",
+                                          .type = ARGUMENT_TYPE_POSITIONAL,
+                                          .required = 1,
+                                      });
     ds_argparse_add_argument(&parser, (ds_argparse_options){
-                                      .short_name = 'f',
-                                      .long_name = "file",
-                                      .description = "just a file",
-                                      .type = ARGUMENT_TYPE_POSITIONAL,
-                                      .required = 0,
-                                  });
+                                          .short_name = 'f',
+                                          .long_name = "file",
+                                          .description = "just a file",
+                                          .type = ARGUMENT_TYPE_POSITIONAL,
+                                          .required = 0,
+                                      });
     ds_argparse_add_argument(&parser, (ds_argparse_options){
-                                      .short_name = 'o',
-                                      .long_name = "output",
-                                      .description = "optional output file",
-                                      .type = ARGUMENT_TYPE_VALUE,
-                                      .required = 0,
-                                  });
+                                          .short_name = 'o',
+                                          .long_name = "output",
+                                          .description = "optional output file",
+                                          .type = ARGUMENT_TYPE_VALUE,
+                                          .required = 0,
+                                      });
+    ds_argparse_add_argument(&parser, (ds_argparse_options){
+                                          .short_name = 'F',
+                                          .long_name = "files",
+                                          .description = "just some files",
+                                          .type = ARGUMENT_TYPE_POSITIONAL_REST,
+                                          .required = 1,
+                                      });
 
     if (ds_argparse_parse(&parser, argc, argv) != 0) {
         return_defer(1);
@@ -43,11 +50,21 @@ int main(int argc, char *argv[]) {
     char *number = ds_argparse_get_value(&parser, "number");
     char *file = ds_argparse_get_value(&parser, "file");
 
+    ds_dynamic_array files;
+    ds_argparse_get_values(&parser, "files", &files);
+
     if (number) {
         printf("number: %s\n", number);
     }
 
     printf("file: %s\n", file);
+
+    for (size_t i = 0; i < files.count; i++) {
+        char *file = NULL;
+        ds_dynamic_array_get(&files, i, &file);
+
+        printf("file: %s\n", file);
+    }
 
 defer:
     ds_argparse_free(&parser);
