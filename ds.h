c57291da2629d675c6de70b18ae73255fa7d66b4
Alex Jercan <jercan_alex27@yahoo.com>
2024-06-01 09:17:08 +0300

    read write files

--- ds.h
+++ ds.h
@@ -25,6 +25,7 @@
 // implementation of the allocator utility and set the allocator to use
 // - DS_AP_IMPLEMENTATION: Define this macro in one source file to include the
 // implementation of the ds_argument parser utility
+// - DS_IO_IMPLEMENTATION: Define this macro for some io utils
 //
 // MEMORY MANAGEMENT
 //
@@ -319,6 +320,16 @@ DSHDEF void ds_argparse_print_help(struct ds_argparse_parser *parser);
 DSHDEF void ds_argparse_print_version(struct ds_argparse_parser *parser);
 DSHDEF void ds_argparse_parser_free(struct ds_argparse_parser *parser);
 
+// IO
+//
+// The io utils are a simple set of utilities to read and write files.
+#ifndef LINE_MAX
+#define LINE_MAX 4096
+#endif
+
+DSHDEF int ds_io_read_file(const char *path, char **buffer);
+DSHDEF int ds_io_write_file(const char *path, const char *buffer, const char *mode);
+
 // RETURN DEFER
 //
 // The return_defer macro is a simple way to return a value and jump to a label
@@ -567,6 +578,10 @@ static inline void *ds_realloc(void *a, void *ptr, unsigned int old_sz,
 #define DS_AL_IMPLEMENTATION
 #define DS_AP_IMPLEMENTATION
 #endif // DS_IMPLEMENTATION
+
+#ifdef DS_IO_IMPLEMENTATION
+#define DS_SB_IMPLEMENTATION
+#endif // DS_IO_IMPLEMENTATION
 
 #ifdef DS_PQ_IMPLEMENTATION
 #define DS_DA_IMPLEMENTATION
@@ -2382,3 +2397,114 @@ void ds_argparse_free(ds_argparse_parser *parser) {
 }
 
 #endif // DS_AP_IMPLEMENTATION
+
+#ifdef DS_IO_IMPLEMENTATION
+
+// Read a file
+//
+// Reads the contents of a file into a buffer.
+//
+// Arguments:
+// - filename: name of the file to read
+// - buffer: pointer to the buffer to store the contents of the file
+//
+// Returns:
+// - the number of bytes read
+DSHDEF int ds_io_read_file(const char *filename, char **buffer) {
+    int result = 0;
+    FILE *file = NULL;
+    ds_string_builder sb;
+    ds_string_builder_init(&sb);
+
+    if (filename != NULL) {
+        file = fopen(filename, "r");
+        if (file == NULL) {
+            DS_LOG_ERROR("Failed to open file: %s", filename);
+            return_defer(-1);
+        }
+    } else {
+        file = stdin;
+    }
+
+    char line[LINE_MAX] = {0};
+    while (fgets(line, sizeof(line), file) != NULL) {
+        unsigned int len = 0;
+        for (len = 0; len < LINE_MAX; len++) {
+            if (line[len] == '\n' || line[len] == EOF) {
+                break;
+            }
+        }
+
+        if (len == LINE_MAX) {
+            len = strlen(line);
+        } else {
+            len += 1;
+            line[len] = '\0';
+        }
+
+        if (len == LINE_MAX) {
+            DS_LOG_ERROR("Line too long");
+            return_defer(-1);
+        }
+
+        if (ds_string_builder_appendn(&sb, line, len) != 0) {
+            DS_LOG_ERROR("Failed to append line to string builder");
+            return_defer(-1);
+        }
+
+        memset(line, 0, sizeof(line));
+    }
+
+    if (ds_string_builder_build(&sb, buffer) != 0) {
+        DS_LOG_ERROR("Failed to build string from string builder");
+        return_defer(-1);
+    }
+
+    result = sb.items.count;
+
+defer:
+    if (filename != NULL && file != NULL)
+        fclose(file);
+    ds_string_builder_free(&sb);
+    return result;
+}
+
+// Write a file
+//
+// Writes the contents of a buffer to a file.
+//
+// Arguments:
+// - filename: name of the file to write
+// - buffer: pointer to the buffer to write to the file
+// - mode: mode to open the file in
+//
+// Returns:
+// - 0 if the write was successful, -1 otherwise
+DSHDEF int ds_io_write_file(const char *filename, const char *buffer, const char *mode) {
+    int result = 0;
+    FILE *file = NULL;
+
+    if (filename != NULL) {
+        file = fopen(filename, mode);
+        if (file == NULL) {
+            DS_LOG_ERROR("Failed to open file: %s", filename);
+            return_defer(-1);
+        }
+    } else {
+        file = stdout;
+    }
+
+    if (fputs(buffer, file) == EOF) {
+        DS_LOG_ERROR("Failed to write to file");
+        return_defer(-1);
+    }
+
+    result = 0;
+
+defer:
+    if (filename != NULL && file != NULL)
+        fclose(file);
+    return result;
+}
+
+#endif // DS_IO_IMPLEMENTATION
